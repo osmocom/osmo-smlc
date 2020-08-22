b6ebda3594eb8aeb1de109a779c98bfb155812fc
Harald Welte <laforge@osmocom.org>
2020-08-22 09:33:47 +0200

    contrib/jenkins.sh: Build libosmo-abis, a dependency of libosmo-netif
    Change-Id: I059434e778954a9491b8d66eff8be3699ddda6af
    Related: OS#4626

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -31,6 +31,7 @@ export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
 export PATH="$inst/bin:$PATH"
 
+osmo-build-dep.sh libosmo-abis
 osmo-build-dep.sh libosmo-netif
 osmo-build-dep.sh libosmo-sccp
 
